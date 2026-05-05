91ae7a026c699c0669d4d7544589f51ad7fc3abd
unknown <codingnik20@gmail.com>
2026-05-05 14:56:09 +0530

    refactor: migrate WebClient to OpenFeign, replace OpenTelemetry with Micrometer Tracing (Brave + Zipkin).

--- src/main/java/com/rating/api/configuration/SecurityConfig.java
+++ src/main/java/com/rating/api/configuration/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.rating.api.rbac.JwtAuthConverter;
+
 @Configuration
 public class SecurityConfig {
 
@@ -12,12 +14,10 @@ public class SecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http.csrf(csrf -> csrf.disable())
-
 				.authorizeHttpRequests(
 						auth -> auth.requestMatchers("/actuator/**").permitAll().anyRequest().authenticated())
-
-				.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
-				}));
+				.oauth2ResourceServer(
+						oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(new JwtAuthConverter())));
 
 		return http.build();
 	}
