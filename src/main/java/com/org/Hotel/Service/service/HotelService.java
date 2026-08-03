57f1ccc95a30f89e72fa68363bdef7851bbbdd17
NikStack20 <codingnik20@gmail.com>
2026-08-03 16:34:59 +0530

    refactor(service): improve hotel service implementation and align dependencies
    Refine service layer implementation and standardize project dependencies.

--- src/main/java/com/org/Hotel/Service/service/HotelService.java
+++ src/main/java/com/org/Hotel/Service/service/HotelService.java
@@ -1,6 +1,8 @@
 package com.org.Hotel.Service.service;
 
-import java.util.List; 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.org.Hotel.Service.loadouts.HotelDto;
 
@@ -9,10 +11,10 @@ public interface HotelService {
 	// create
 	HotelDto createHotel(HotelDto hotelDto);
 
-	// getAll
-	List<HotelDto> getAllHotels();
-
 	// getSingleHotel
 	HotelDto getHotel(String hotelId);
+
+    // get hotels by userId
+    Map<String, HotelDto> getHotelsByHotelIds(Set<String> hotelIds);
 
 }
